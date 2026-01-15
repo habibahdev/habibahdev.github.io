495fb522e3d7da7652b1d24901cf8c4f7ce6e63e
habibahdev <ummhabibah-ouattara@outlook.com>
2026-01-15 15:40:11 +0100

    update dark/light mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,21 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        navy: '#0a192f',
-        lightNavy: '#112240',
-        slate: '#8892b0',
-        lightSlate: '#ccd6f6',
-        accent: '#a8dadc',
+        //navy: '#0a192f',
+        //lightNavy: '#112240',
+        //slate: '#8892b0',
+        //lightSlate: '#ccd6f6',
+        //accent: '#a8dadc',
+        navy: '#0f172a', // bleu nuit => background principal
+        lightNavy: '#1e293b', // bleu gris => background secondaire
+        slate: '#94a3b8', // gris bleuté (texte secondaire)
+        lightSlate: '#f1f5f9', // girs très clair (texte principal)
+        accent: '#06b6d4', // cyan (accent dark)
+        lightBg: '#f8fafc', // gris très claire (background principal)
+        lightBgSecondary: '#f1f5f9', // gris claire (background secondaire)
+        lightText: '#1e293b', // gris foncé (texte principal)
+        lightTextSecondary: '#64748b', // gris (texte secondaire)
+        accentLight: '#8b5cf6' // violet => accent light
       },
     }
   },
